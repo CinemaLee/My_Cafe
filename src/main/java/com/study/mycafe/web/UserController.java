08f6eeb08bb6ed4352a36d87a147c337196c6f40
lee <jif0631@gmail.com>
2019-11-11 12:35:55 +0900

    로그인 메뉴 관리 및 로그 아웃 기능 구현

--- src/main/java/com/study/mycafe/web/UserController.java
+++ src/main/java/com/study/mycafe/web/UserController.java
@@ -1,33 +1,68 @@
 package com.study.mycafe.web;
 
+import com.study.mycafe.dto.UserDto;
 import com.study.mycafe.repository.UserRepository;
 import com.study.mycafe.domain.User;
 import com.study.mycafe.exception.PersonNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
 
-    @GetMapping("/login")
+    @GetMapping("/loginForm")
     public String loginForm() {
         return "user/login";
     }
 
 
-    @GetMapping("/register")
+    @PostMapping("/login")
+    public String login(String userId, String password, HttpSession session, Model model) {
+
+        User user = userRepository.findByUserId(userId);
+
+        if(user == null) {
+            log.info("Login Fail.");
+            model.addAttribute("message","존재하지 않는 아이디입니다.");
+            return "redirect:/user/loginForm";
+        }
+
+        if(!user.getPassword().equals(password)){
+            log.info("Login Fail.");
+            model.addAttribute("message","비밀번호가 틀렸습니다.");
+            return "redirect:/user/loginForm";
+        }
+        log.info("Login Success!!");
+        log.info("LoginUser : >> "+user);
+        session.setAttribute("loginUser",user); // 세션에 저장.
+
+
+
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("loginUser");
+        return "redirect:/";
+    }
+
+
+
+    @GetMapping("/registerForm")
     public String registerForm() {
         return "user/registerForm";
     }
@@ -45,17 +80,32 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(Model model) {
-        List<User> all = userRepository.findAll();
+        List<User> all = userRepository.findAll(); // 이럴경우 굳이 service를 활용해야 하는가에 대해 고민해봐도 괜찮다.
         model.addAttribute("users",all);
         return "user/list"; // templates
     }
 
 
-    @GetMapping("/{id}/form")
+    @GetMapping("/{id}/updateForm")
     public String updateForm(@PathVariable Long id, Model model) {
+
         User user = userRepository.findById(id).orElseThrow(PersonNotFoundException::new);
 
         model.addAttribute("users",user);
         return "user/updateForm";
     }
+
+    @PutMapping("/{id}/update")
+    public String update(@PathVariable Long id, UserDto userDto){
+
+        User userAtDb = userRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+        userAtDb.setUserId(userDto.getUserId());
+        userAtDb.setName(userDto.getName());
+        userAtDb.setEmail(userDto.getEmail());
+        userAtDb.setPassword(userDto.getPassword());
+        userRepository.save(userAtDb);
+
+        return "redirect:/user/list";
+    }
+
 }
