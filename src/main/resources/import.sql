379c300ff77fbadfe13a730e6b67eea4b164b834
lee <jif0631@gmail.com>
2019-11-18 21:37:44 +0900

    오류테스트3

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 
 
-insert into user(`id`, `user_Id`, `password`, `name`, `email` ,`img_code`, `created_at`) values (1,'lee','123','이강우','lee@na.com','/images/이강우50.PNG',current_timestamp());
-insert into user(`id`, `user_Id`, `password`, `name`, `email`,`img_code`, `created_at` ) values (2,'kim','123','김민수','kim@na.com','/images/guest50.PNG',current_timestamp());
+insert into user(`id`, `user_Id`, `password`, `name`, `email` ,`img_code`, `created_at`) values (1,'lee','123','이강우','lee@na.com','http://localhost:8080/images/이강우50.PNG',current_timestamp());
+insert into user(`id`, `user_Id`, `password`, `name`, `email`,`img_code`, `created_at` ) values (2,'kim','123','김민수','kim@na.com','http://localhost:8080/images/guest50.PNG',current_timestamp());
 
 
 insert into question(`id`,`title`,`contents`,`created_at`,`count_Of_Answer`, `user_idx`) values (1,'첫번째 질문','인간은 태어날때부터 악한가요?',current_timestamp() ,0,1);
