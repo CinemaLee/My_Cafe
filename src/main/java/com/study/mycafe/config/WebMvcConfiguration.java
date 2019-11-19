bda0d5c403f4267e4014f48704228d68cd52ac0d
lee <jif0631@gmail.com>
2019-11-19 15:00:06 +0900

    썸네이 외부url에서 가져오기

---
+++ src/main/java/com/study/mycafe/config/WebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.study.mycafe.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+//
+//    @Value("${resources.location}")
+//    private String resourcesLocation;
+//
+//    @Value("${resources.uri_path:}")
+//    private String resourcesUriPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+//                .addResourceLocations("file:///" + "C:/Users/owner/imagedata/");
+                  .addResourceLocations("file:///" + "https://drive.google.com/drive/folders/1Nzt9oYCtCtQCNSokG8bmutzb69PxjomU");
+    }
+}
