c924e397ed55c79fa6ff5b6959225f1b3caf18c5
lee <jif0631@gmail.com>
2019-11-19 19:22:03 +0900

    구글드라이브 테스트

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 
 
-insert into user(`id`, `user_Id`, `password`, `name`, `email` ,`img_code`, `created_at`) values (1,'lee','123','이강우','lee@na.com','/images/이강우50.png',current_timestamp());
-insert into user(`id`, `user_Id`, `password`, `name`, `email`,`img_code`, `created_at` ) values (2,'kim','123','김민수','kim@na.com','/images/guest50.png',current_timestamp());
+insert into user(`id`, `user_Id`, `password`, `name`, `email` ,`img_code`, `created_at`) values (1,'lee','123','이강우','lee@na.com','http://drive.google.com/uc?export=view&id=1n6AFP9iwROmqXAa7V832yeJKrKBp4mNc',current_timestamp());
+insert into user(`id`, `user_Id`, `password`, `name`, `email`,`img_code`, `created_at` ) values (2,'kim','123','김민수','kim@na.com','http://drive.google.com/uc?export=view&id=16WHxGxAQ6yhmr_TEZRc2lFDDW994G20a',current_timestamp());
 
 
 insert into question(`id`,`title`,`contents`,`created_at`,`count_Of_Answer`, `user_idx`) values (1,'첫번째 질문','인간은 태어날때부터 악한가요?',current_timestamp() ,0,1);
